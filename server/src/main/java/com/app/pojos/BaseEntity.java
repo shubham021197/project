f8e38e979d4b939d795ec28d80f123da50ef7e63
shubham <shubham@Madlap>
2023-08-17 20:00:22 +0530

    server added

---
+++ server/src/main/java/com/app/pojos/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.app.pojos;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public class BaseEntity{ 
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+}
